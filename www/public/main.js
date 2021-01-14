fd203c76fdd86822e1cef61d078a7bacab430afb
Ubuntu <saidameramayas@gmail.com>
2021-01-14 16:51:00 -0500

    Update frontend

--- www/public/main.js
+++ www/public/main.js
@@ -1,8 +1,16 @@
 const postBtn = document.getElementById("postImageBtn");
 const searchBtn = document.getElementById("searchImagesBtn");
 
+const resultSection = document.getElementById("res");
+const postMsg = document.getElementById("postMsg");
+
+const imageInput = document.getElementById("imageInput");
+
+imageInput.onchange = () => {
+    postMsg.innerText = "";
+};
+
 function renderImages(metadata) {
-    const resultSection = document.getElementById("res");
     resultSection.innerHTML = "";
     metadata.forEach((value) => {
         const divElement = document.createElement("div");
@@ -11,6 +19,14 @@ function renderImages(metadata) {
         const imgElement = document.createElement("img");
         const imgSRC = "/api/images/" + value["Name"] + "." + value["Format"];
         imgElement.setAttribute("src", imgSRC);
+        let w = value["Width"];
+        let h = value["Height"];
+        while ((w > 200) && (h > 200)) {
+            w = 0.9 * w;
+            h = 0.9 * h;
+        }
+        imgElement.setAttribute("width", w);
+        imgElement.setAttribute("height", h);
         const deleteBtnElement = document.createElement("button");
         deleteBtnElement.innerText = "Delete";
         deleteBtnElement.onclick = () => {
@@ -21,20 +37,29 @@ function renderImages(metadata) {
                 searchBtn.onclick()
             });
         };
-        const brElement = document.createElement("br");
+        divElement.appendChild(document.createElement("br"));
         divElement.appendChild(deleteBtnElement);
+        divElement.appendChild(document.createElement("br"));
         divElement.appendChild(imgElement);
-        divElement.appendChild(brElement);
+        divElement.appendChild(document.createElement("br"));
         resultSection.appendChild(divElement);
+        divElement.appendChild(document.createElement("br"));
     });
 }
 
 postBtn.onclick = () => {
+    postMsg.innerText = "Adding image...";
     fetch("/api/images", {
         method: "POST",
-        body: document.getElementById("imageInput").files[0]
-    }).then(data => data.json()).then(result => console.log(result));
-}
+        body: imageInput["files"][0]
+    }).then((result) => {
+        if (result.status === 200) {
+            postMsg.innerText = "Image added";
+        } else {
+            postMsg.innerText = "Unable to add image";
+        }
+    });
+};
 
 searchBtn.onclick = () => {
     let url = "/api/images/metadata?";
@@ -47,4 +72,4 @@ searchBtn.onclick = () => {
     fetch(url).then(data => data.json()).then((result) => {
         renderImages(result)
     })
-}
+};
